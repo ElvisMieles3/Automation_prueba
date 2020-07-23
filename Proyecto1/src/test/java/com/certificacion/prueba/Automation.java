b11a6f6fc05ac56ad8fedac357600605aaaa77ca
ElvisMieles3 <elvismieles@gmail.com>
2020-07-23 01:19:46 -0500

    Se terminar el proyecto 1

--- Proyecto1/src/test/java/com/certificacion/prueba/Automation.java
+++ Proyecto1/src/test/java/com/certificacion/prueba/Automation.java
@@ -7,9 +7,7 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -24,6 +22,14 @@ public class Automation {
     By linkByHtml = By.linkText("linkByHtml");
 
     By selects = By.linkText("Select Test");
+    By iframe = By.linkText("IFrames Test");
+    By clicks = By.linkText("Clicks Page");
+
+    By alerts = By.linkText("Alert Test");
+    By promptTest = By.linkText("Prompt Page");
+    By scroll  = By.linkText("Form Test");
+
+
 
     @Before
     public void setUp() throws Exception {
@@ -67,6 +73,89 @@ public class Automation {
 
         Select s1 =  new Select (driver.findElement(By.id("s1")));
         s1.selectByVisibleText("Business Phone");
-        
+
     }
+
+    @Test
+    public void Iframes ()  {
+
+        driver.findElement(iframe).click();
+        /// FALTA
+
+    }
+
+    @Test
+    public void Clicks  ()  {
+
+        driver.findElement(clicks).click();
+
+        driver.findElement(By.xpath("//input[@value='click me']")).click();
+        driver.findElement(By.xpath("//input[@value='dbl click me']")).click();
+        driver.findElement(By.xpath("//input[@value='right click me']")).click();
+
+    }
+
+    @Test
+    public void Alerts  ()  {
+
+        driver.findElement(alerts).click();
+
+        driver.findElement(By.xpath("//input[@value='Click For Alert']")).click();
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.xpath("//input[@value='Click For Multiline Alert']")).click();
+        driver.switchTo().alert().accept();
+
+
+    }
+
+
+    @Test
+    public void PromptTest ()  {
+
+        driver.findElement(promptTest).click();
+
+        driver.findElement(By.name("b1")).click();
+        driver.switchTo().alert().sendKeys("Hola Mundo!");
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.name("b1")).click();
+        Alert alert = driver.switchTo().alert();
+        String mensajeAlerta = alert.getText();
+        System.out.println(mensajeAlerta);
+        alert.accept();
+
+    }
+
+    @Test
+    public void NuevaPestana ()  {
+
+
+        //////FALTA
+
+    }
+
+    @Test
+    public void VentanasEmergentes ()  {
+
+       // FALTA
+    }
+
+
+    @Test
+    public void MoverScroll  ()  {
+
+        driver.findElement(scroll).click();
+        driver.switchTo().alert().accept();
+        JavascriptExecutor ScrollBy = (JavascriptExecutor) driver;
+
+        ScrollBy.executeScript("window.scrollBy(0,1000)");
+
+        JavascriptExecutor ScrollBy2 = (JavascriptExecutor) driver;
+
+        ScrollBy2.executeScript("window.scrollBy(1000,0)");
+
+    }
+
+
 }
